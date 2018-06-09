e63d7c9124a8e9aa9c8bbcdb749e36660a1fbb25
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-06-09 18:31:33 +0200

    :sparkles: Implement restore functionality

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ const os = require('os');
 const shell = require('shelljs');
 const prompts = require('prompts');
 const program = require('commander');
-const { name, description, version } = require('./package.json');
+const { description, version } = require('./package.json');
 
+// TODO: Refactor into dedicated files
 const getTimestamp = () => Math.round(new Date() / 1000);
 const getHomeDirectory = () => os.homedir();
 const getDesktopDirectory = () => [getHomeDirectory(), 'Desktop'].join('/');
-const getRepositoryPath = () => [getHomeDirectory(), name].join('/');
+const getRepositoryPath = () =>
+  [getHomeDirectory(), 'Desktop-CLI', 'backups'].join('/');
 const getDirectoryFilesCount = path => shell.ls(path).length;
 const isDirectoryEmpty = path => getDirectoryFilesCount(path) === 0;
-const constructStashDirectory = () =>
-  [getRepositoryPath(), getTimestamp()].join('/');
+const constructBackupDirectory = backupId =>
+  backupId
+    ? [getRepositoryPath(), backupId].join('/')
+    : [getRepositoryPath(), getTimestamp()].join('/');
 const ifElse = (ifCondition, thenValue, elseValue) =>
   ifCondition ? thenValue : elseValue;
+
+// TODO: Account for all file system errors (e.g. permission denial)
+// TODO: Investigate feasibility of avoiding using moving files (& linking instead)
+// TODO: Decide on indexing strategy (e.g. UNIX timestamps, incremental integers)
 
 program
   .version(version)
@@ -45,6 +53,8 @@ program
       const desktopFilesCount = getDirectoryFilesCount(desktopDirectory);
 
       shell.rm('-rf', [desktopDirectory, '*'].join('/'));
+
+      // TODO: Refactor pluralization
       shell.echo(
         desktopFilesCount +
           ' Desktop file' +
@@ -54,22 +64,56 @@ program
     }
   });
 
+// TODO: Add backup naming functionality
+
 program
-  .command('stash')
-  .description('')
+  .command('backup')
+  .description('Backup Desktop files')
   .action(() => {
     const desktopDirectory = getDesktopDirectory();
-    const stashDirectory = constructStashDirectory();
+    const backupDirectory = constructBackupDirectory();
 
     if (isDirectoryEmpty(desktopDirectory)) {
-      shell.echo('Cannot stash empty Desktop');
+      shell.echo('Cannot backup empty Desktop');
       shell.exit(1);
     }
 
-    shell.mkdir('-p', stashDirectory);
-    shell.mv([desktopDirectory, '*'].join('/'), stashDirectory);
-    shell.echo('Saved Desktop contents in ' + stashDirectory);
+    shell.mkdir('-p', backupDirectory);
+    shell.mv([desktopDirectory, '*'].join('/'), backupDirectory);
+    shell.echo('Stored Desktop backup in ' + backupDirectory);
   });
+
+// TODO: Add restore last functionality
+// TODO: Add backup listing functionality
+
+program
+  .command('restore <backupId>')
+  .description('Restore Desktop backup')
+  .action(backupId => {
+    const desktopDirectory = getDesktopDirectory();
+    const backupDirectory = constructBackupDirectory(backupId);
+
+    if (!isDirectoryEmpty(desktopDirectory)) {
+      shell.echo('Cannot overwrite non-empty Desktop');
+      shell.exit(1);
+    }
+
+    // TODO: Check if backup exists
+
+    const backupFilesCount = getDirectoryFilesCount(backupDirectory);
+    shell.mv([backupDirectory, '*'].join('/'), desktopDirectory);
+
+    // TODO: Refactor pluralization
+    shell.echo(
+      'Restored ' +
+        backupFilesCount +
+        ' file' +
+        ifElse(backupFilesCount === 1, '', 's') +
+        ' from backup',
+    );
+  });
+
+// TODO: Add quick note-taking functionality
 
 program.parse(process.argv);
 
