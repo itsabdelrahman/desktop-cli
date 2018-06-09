a63b25263d219134837a9992d9eede03124d9359
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-06-09 04:49:38 +0200

    :sparkles: Implement stash functionality

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 #! /usr/bin/env node
 'use strict';
 
+const os = require('os');
+const shell = require('shelljs');
 const program = require('commander');
-const { description, version } = require('./package.json');
+const { name, description, version } = require('./package.json');
+
+const getTimestamp = () => Math.round(new Date() / 1000);
+const getHomeDirectory = () => os.homedir();
+const getDesktopDirectory = () => [getHomeDirectory(), 'Desktop'].join('/');
+const getRepositoryPath = () => [getHomeDirectory(), name].join('/');
+const constructStashDirectory = () =>
+  [getRepositoryPath(), getTimestamp()].join('/');
 
 program
   .version(version)
   .description(description)
   .usage('<command> [args]');
+
+program
+  .command('stash')
+  .description('')
+  .action(() => {
+    const desktopDirectory = getDesktopDirectory();
+    const stashDirectory = constructStashDirectory();
+
+    shell.mkdir('-p', stashDirectory);
+    shell.mv([desktopDirectory, '*'].join('/'), stashDirectory);
+  });
 
 program.parse(process.argv);
