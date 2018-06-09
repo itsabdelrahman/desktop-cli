018dfbc086af778eabc0e689cb4fb1a13c651eb9
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-06-09 05:03:52 +0200

    :hammer: Account for empty desktop

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const getTimestamp = () => Math.round(new Date() / 1000);
 const getHomeDirectory = () => os.homedir();
 const getDesktopDirectory = () => [getHomeDirectory(), 'Desktop'].join('/');
 const getRepositoryPath = () => [getHomeDirectory(), name].join('/');
+const isDirectoryEmpty = path => shell.ls(path).length === 0;
 const constructStashDirectory = () =>
   [getRepositoryPath(), getTimestamp()].join('/');
 
@@ -25,8 +26,14 @@ program
     const desktopDirectory = getDesktopDirectory();
     const stashDirectory = constructStashDirectory();
 
+    if (isDirectoryEmpty(desktopDirectory)) {
+      shell.echo('Cannot stash empty desktop');
+      shell.exit(1);
+    }
+
     shell.mkdir('-p', stashDirectory);
     shell.mv([desktopDirectory, '*'].join('/'), stashDirectory);
+    shell.echo('Saved desktop contents in ' + stashDirectory);
   });
 
 program.parse(process.argv);
